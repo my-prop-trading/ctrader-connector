aa7936a75fbf44a7bf38261b07cb314f2738b9b7
Maxim Gorin <onlinexmax@gmail.com>
2024-07-15 17:38:45 +0300

    Add create token models

--- src/rest/endpoints.rs
+++ src/rest/endpoints.rs
@@ -2,12 +2,14 @@ use http::Method;
 
 pub enum CtraderEndpoint {
     CreateManagerToken,
+    CreateCTID
 }
 
 impl From<&CtraderEndpoint> for String {
     fn from(item: &CtraderEndpoint) -> Self {
         match item {
             CtraderEndpoint::CreateManagerToken => "/webserv/managers/token".to_string(),
+            CtraderEndpoint::CreateCTID => "/ctid/create".to_string(),
         }
     }
 }
@@ -16,6 +18,7 @@ impl CtraderEndpoint {
     pub fn get_http_method(&self) -> Method {
         match &self {
             CtraderEndpoint::CreateManagerToken => Method::POST,
+            CtraderEndpoint::CreateCTID => Method::POST,
         }
     }
 }
--- src/rest/models.rs
+++ src/rest/models.rs
@@ -1,3 +1,17 @@
 use serde::Serialize;
+use serde_derive::Deserialize;
 
 pub trait CtraderRequest: Serialize {}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct CreateCtraderManagerTokenRequest {
+    pub login: String,
+    #[serde(rename = "hashedPassword")]
+    pub hashed_password: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct CreateCtraderManagerTokenResponse {
+    #[serde(rename = "webservToken")]
+    pub token: String,
+}
